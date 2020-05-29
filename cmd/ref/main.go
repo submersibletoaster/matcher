59c4c8b25d29335daccf16c28f36cf55ef0fae98
Andrew Bramble <andrew.bramble@katana1.com>
2020-05-29 15:46:05 +1000

    Reference search glyphs against themselves. Problems with small numbers of set bits

--- cmd/ref/main.go
+++ cmd/ref/main.go
@@ -2,34 +2,39 @@ package main
 
 import (
 	"fmt"
+
+	log "github.com/sirupsen/logrus"
 	"github.com/submersibletoaster/matcher/glyph"
 	"github.com/submersibletoaster/matcher/unscii"
-	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	log.SetLevel(log.DebugLevel)
+//	log.SetLevel(log.DebugLevel)
 	font := unscii.Font
-	
-	
+
+	// ASCII only
 	/*
-	chars := make([]rune,0)
-	for i := 0x20 ; i <= 0x7E ; i++ {
-		chars=append(chars,rune(i))
-	}
+		chars := make([]rune, 0)
+
+		for i := 0x20; i <= 0x7E; i++ {
+			chars = append(chars, rune(i))
+		}
 	*/
+
 	cm := unscii.CharMap()
-	chars := make([]rune,len(cm))
-	n := 0
-	for k,_ := range cm {
-		chars[n] = rune(k)
-		n++
+	chars := make([]rune, 0)
+	for k, _ := range cm {
+		if glyph.UsablePoint(k) {
+			chars = append(chars, rune(k))
+		}
 	}
-	g := glyph.NewRasterFont(font,chars)
-	log.Printf("%#+v\n",g)
+	fmt.Printf("%d usable unicode codepoints\n", len(chars))
+
+	g := glyph.NewRasterFont(font, chars)
 	perfect := 0
 	edge := 0
-	for _,c := range chars {
+	//chars = []rune{''}
+	for _, c := range chars {
 		r := g.Query(g.ImageForRune(c))
 		//r = r[len(r)-5:len(r)-1]
 		r = r[0:3]
@@ -38,13 +43,13 @@ func main() {
 			continue
 		}
 		edge++
-		fmt.Printf("%s\t", string(c) )
-		for _,v := range r {
-			fmt.Printf("%.5f,'%s'\t",v.Score,string(v.Rune))
+		fmt.Printf("'%s'\t%x\t", string(c), c)
+		for _, v := range r {
+			fmt.Printf("%.5f,'%s'\t", v.Score, string(v.Rune))
 		}
 		fmt.Println()
 	}
 
-	log.Infof("Perfect 1st match %d , edge cases %d\n", perfect, edge )
+	log.Infof("Perfect 1st match %d , edge cases %d\n", perfect, edge)
 
 }
